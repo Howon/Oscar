5848fd4466e219f661edb441ad93bdd9ad3fd907
Howon <howonbyun@gmail.com>
2016-10-21 03:13:57 -0400

    consistent naming convention

--- src/ast.mli
+++ src/ast.mli
@@ -1,34 +1,83 @@
-type op = Add | Sub | Mult | Div | Equal | Neq |
-Less | Leq | Greater | Geq | And | Or | Send
-type uop = Neg | Not
-type typ = Int | Bool | Void
-type bind = typ * string
+type bin_op =
+    | Add    | Minus | Multiply | Divide
+    | Modulo | Equal | Neq      | Lt
+    | Leq    | Gt    | Geq      | And
+    | Or
+
+type u_op = Not | Neg
+
+type p_type = Int_t | Bool_t | Double_t | Char_t | Unit_t
+
+type cont_type = String_t | List_t | Set_t | Map_t | Tuple_t
+
+type opt_type = Maybe_t of types | Some_t of types | None_t
+
+type message_type = Message_t
+
+type field = Field of types * string
+
+type func = {
+    func_name : string;
+    function_return_t : types;
+    function_formals : formal list;
+    function_body : stmt list;
+}
+
+type actor = {
+    actor_fields : field list;
+    actor_name : string;
+    acator_receive: receive;
+}
+
+type actor_type = Actor_t of string
+
+type pool_type = Pool_t of actor_type list
+
+type types =
+    | Primitive of p_type
+    | Container of cont_type
+    | Optional of opt_type
+    | Actor_t of actor_op
+
+type bind_type = types * string
+
+type bit_op =
+    | Big_RShift
+    | Bit_LShift
+    | Bit_And
+    | Bit_Or
+    | Bit_Xor
+    | Bit_Not
+
+type actor_op =
+    | Actor_send
+    | Actor_broadcast
+    | Actor_receive
 
 type expr =
-    | Literal of int
-    | BoolLit of bool
+    | Binop of expr * bin_op * expr
+    | Bitop of int * bit_op * int
+    | Uop of u_op * expr
     | Id of string
-    | Binop of expr * op * expr
-    | Unop of uop * expr
-    | Assign of string * expr
-    | Call of string * expr list
+    | Assign of expr * expr
+    | Int_lit of int
+    | Bool_lit of bool
+    | Double_lit of float
+    | Char_lit of char
+    | String_lit of string
+    | List_init of expr list
+    | Function_call of string * expr list
+    | Actor_comm of message_type * actor_op * actor_type
     | Noexpr
 
 type stmt =
     | Block of stmt list
     | Expr of expr
+    | Return of expr
     | If of expr * stmt * stmt
     | For of expr * expr * expr * stmt
     | While of expr * stmt
-    | Return of expr
+    | Break
+    | Continue
 
-type func_decl = {
-    typ : typ;
-    fname : string;
-    formals : bind list;
-    locals : bind list;
-    body : stmt list;
 
-}
-
-type program = bind list * func_decl list
