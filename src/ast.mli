e761d94574e35f225305f22974800c370d1352af
Vlad Scherbich <vscherbich@squarespace.com>
2016-10-30 11:44:25 -0400

    ast: brand new day with a brand new branch

--- src/ast.mli
+++ src/ast.mli
@@ -1,9 +1,9 @@
 type program = None
 
 type bin_op =
-  | Add    | Minus | Multiply | Divide
-  | Modulo | Equal | Neq      | Lt
-  | Leq    | Gt    | Geq      | And
+    Add | Sub | Mult | Div
+  | Mod | Equal | Neq | Less
+  | Leq | Greater | Geq | And
   | Or
 
 type u_op = Not | Neg
@@ -12,56 +12,50 @@ type p_type = Int_t | Bool_t | Double_t | Char_t | Unit_t
 
 type cont_type = String_t | List_t | Set_t | Map_t | Tuple_t
 
-type opt_type = Maybe_t of types | Some_t of types | None_t
+type actor_op =
+    Actor_send
+  | Actor_broadcast
+  | Actor_receive
 
-type message_type = Message_t
+type types =
+    Primitive of p_type
+  | Container of cont_type
+  | None_type
+
+type opt_type = Maybe_t | Some_t | None_t
+
+type optional = {
+    type_: opt_type;
+    param: types;
+}
+
+type formal = string * types
 
 type field = Field of types * string
-
-type func = {
-  func_name : string;
-  function_return_t : types;
-  function_formals : formal list;
-  function_body : stmt list;
-}
-
-type actor = {
-  actor_fields : field list;
-  actor_name : string;
-  acator_receive: receive;
-}
 
 type actor_type = Actor_t of string
 
 type pool_type = Pool_t of actor_type list
 
-type types =
-  | Primitive of p_type
-  | Container of cont_type
-  | Optional of opt_type
-  | Actor_t of actor_op
-
-type bind_type = types * string
-
 type bit_op =
-  | Big_RShift
+    Big_RShift
   | Bit_LShift
   | Bit_And
   | Bit_Or
   | Bit_Xor
   | Bit_Not
 
-type actor_op =
-  | Actor_send
-  | Actor_broadcast
-  | Actor_receive
+type message_type = {
+    name: string;
+    body: stmt list;
+}
 
-type expr =
-  | Binop of expr * bin_op * expr
+and expr =
+    Binop of expr * bin_op * expr
   | Bitop of int * bit_op * int
   | Uop of u_op * expr
   | Id of string
-  | Assign of expr * expr
+  | Assign of string * expr
   | Int_lit of int
   | Bool_lit of bool
   | Double_lit of float
@@ -72,8 +66,8 @@ type expr =
   | Actor_comm of message_type * actor_op * actor_type
   | Noexpr
 
-type stmt =
-  | Block of stmt list
+and stmt =
+    Block of stmt list
   | Expr of expr
   | Return of expr
   | If of expr * stmt * stmt
@@ -81,3 +75,17 @@ type stmt =
   | While of expr * stmt
   | Break
   | Continue
+
+type func = {
+  name : string;
+  return_t : types;
+  formals : formal list;
+  body : stmt list;
+}
+
+type actor = {
+  fields : field list;
+  name : string;
+  formals : formal list;
+  receive: message_type list;
+}
