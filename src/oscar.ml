8348cb5d3ebd150dfa47282e93924f95209a5480
Vlad Scherbich <vscherbich@squarespace.com>
2016-11-12 18:17:14 -0500

    breaking out  fxn into a separate module (again)

---
+++ src/oscar.ml
@@ -0,0 +1,52 @@
+open Ast
+
+let rec eval = function
+      Id(s) -> s
+    | Int_Lit(x) -> x
+    | Double_Lit(x) -> x
+    | Char_Lit(x) -> x
+    | String_Lit(x) -> x
+    | Bool_Lit(x) -> x
+    | Binop(e1, op, e2) ->
+        let v1 = eval e1 and v2 = eval e2 in
+        (
+          match op with
+              Add -> v1 + v2
+            | Sub -> v1 - v2
+            | Mult -> v1 * v2
+            | Div -> v1 / v2
+            | Mod -> v1 % v2
+            | Equal -> v1 = v2
+            | Neq -> v1 <> v2
+            | Less -> v1 < v2
+            | Leq -> v1 <= v2
+            | Greater -> v1 > v2
+            | Geq -> v1 >= v2
+            | And -> v1 && v2
+            | Or -> v1 || v2
+            | Bit_And -> v1 & v2
+            | Bit_Or -> v1 | v2
+            | Bit_Xor -> v1 ^ v2
+            | Bit_RShift -> v1 >> v2
+            | Bit_LShift -> v1 << v2
+        )
+    | Unop(op, e) ->
+        let v = eval e in
+        (
+          match op with
+              Neg -> -v
+            | Not -> !v
+            | Bit_Not -> ~v
+        )
+    | Assign(x, e) ->  x ^ " = " ^ eval e
+    (* todo: make it right
+    | Call(f, el) ->
+          f ^ "(" ^ String.concat ", " (List.map eval el) ^ ")"
+    *)
+    | Noexpr -> ""
+
+let _ =
+    let lexbuf = Lexing.from_channel stdin in
+    let expr = Parser.expr Scanner.token lexbuf in
+    let result = eval expr in
+    print_endline (string_of_int result)
