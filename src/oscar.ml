766baca583054b5cf258fd86ad77be935091296f
Vlad Scherbich <vscherbich@squarespace.com>
2016-11-15 23:15:08 -0500

    broke up eval into different fxns based on var type

--- src/oscar.ml
+++ src/oscar.ml
@@ -3,37 +3,19 @@ open Ast
 let rec eval_string = function
       Id(x) -> x  (* todo: or Id(x) should return the variable's value from symbol table? *)
     | String_Lit(x) -> x
-    (* | Binop(e1, op, e2) ->
-        let v1 = eval_string e1 and v2 = eval_string e2 in
-        (
-          match op with
-              Equal -> "foo"
-              (* Equal -> v1 = v2 *)
-            (* todo: or != instead?? *)
-            | Neq -> v1 <> v2
-            | Less -> v1 < v2
-            | Leq -> v1 <= v2
-            | Greater -> v1 > v2
-            | Geq -> v1 >= v2
-        ) *)
     (* todo: make it right *)
-    (* | Assign(x, e) -> symbol_table[x] = (eval_string e) *)
+    (* 
+        | Assign(x, e) -> symbol_table[x] = (eval_string e)
+        | Call(f, el) -> smth smth
+    *)
 
 let rec eval_char = function
       Char_Lit(x) -> x
-(*     | Binop(e1, op, e2) ->
-        let v1 = eval_char e1 and v2 = eval_char e2 in
-        (
-          match op with
-              Equal -> v1 = v2
-            | Neq -> v1 <> v2
-            | Less -> v1 < v2
-            | Leq -> v1 <= v2
-            | Greater -> v1 > v2
-            | Geq -> v1 >= v2
-        ) *)
     (* todo: make it right *)
-    (* | Assign(x, e) -> symbol_table[x] = (eval_char e) *)
+    (* 
+        | Assign(x, e) -> symbol_table[x] = (eval_string e)
+        | Call(f, el) -> smth smth
+    *)
 
 let rec eval_bool = function
       Bool_Lit(x) -> x
@@ -54,7 +36,10 @@ let rec eval_bool = function
             Not -> not v
         )
     (* todo: make it right *)
-    (* | Assign(x, e) -> symbol_table[x] = (eval_bool e) *)
+    (* 
+        | Assign(x, e) -> symbol_table[x] = (eval_string e)
+        | Call(f, el) -> smth smth
+    *)
 
 let rec eval_int = function
       Int_Lit(x) -> x
@@ -67,13 +52,6 @@ let rec eval_int = function
             | Mult -> v1 * v2
             | Div -> v1 / v2
             | Mod -> v1 mod v2
-            (* | Equal -> v1 = v2
-            (* todo: or != instead?? *)
-            | Neq -> v1 <> v2
-            | Less -> v1 < v2
-            | Leq -> v1 <= v2
-            | Greater -> v1 > v2
-            | Geq -> v1 >= v2 *)
             | Bit_And -> v1 land v2
             | Bit_Or -> v1 lor v2
             | Bit_Xor -> v1 lxor v2
@@ -87,7 +65,10 @@ let rec eval_int = function
               Neg -> -v
         )
     (* todo: make it right *)
-    (* | Assign(x, e) -> symbol_table[x] = (eval_int e) *)
+    (* 
+        | Assign(x, e) -> symbol_table[x] = (eval_string e)
+        | Call(f, el) -> smth smth
+    *)
 
 let rec eval_double = function
       Double_Lit(x) -> x
@@ -100,13 +81,6 @@ let rec eval_double = function
             | Sub -> v1 -. v2
             | Mult -> v1 *. v2
             | Div -> v1 /. v2
-            (* | Equal -> v1 = v2
-            (* todo: or != instead?? *)
-            | Neq -> v1 <> v2
-            | Less -> v1 < v2
-            | Leq -> v1 <= v2
-            | Greater -> v1 > v2
-            | Geq -> v1 >= v2 *)
         )
     | Unop(op, e) ->
         let v = eval_double e in
@@ -115,8 +89,10 @@ let rec eval_double = function
               Neg -> -.v
         )
     (* todo: make it right *)
-    (* | Assign(x, e) -> symbol_table[x] = (eval_double e) *)
-
+    (* 
+        | Assign(x, e) -> symbol_table[x] = (eval_string e)
+        | Call(f, el) -> smth smth
+    *)
 
 let _ =
     let lexbuf = Lexing.from_channel stdin in
