59b3c5b30266dbb6b040876d39b676cc426a73e3
Ethan Adams <ea2678@columbia.edu>
2016-11-19 13:50:08 -0500

    making vlad happy

--- src/codegen.ml
+++ src/codegen.ml
@@ -1,17 +1,3 @@
-(* Code generation: translate takes a semantically checked AST and
-produces LLVM IR
-
-LLVM tutorial: Make sure to read the OCaml version of the tutorial
-
-http://llvm.org/docs/tutorial/index.html
-
-Detailed documentation on the OCaml LLVM library:
-
-http://llvm.moe/
-http://llvm.moe/ocaml/
-
-*)
-
 module L = Llvm
 module A = Ast
 
