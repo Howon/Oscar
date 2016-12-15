722a4984cf423b4b56ad384c7e5ec0ede73f950d
Jibben Hillen <jlh2128@columbia.edu>
2016-12-15 05:49:15 -0500

    better syntax errors for parsing

--- src/oscar.ml
+++ src/oscar.ml
@@ -2,9 +2,22 @@
    check the resulting AST, generate LLVM IR, and dump the module *)
 
 open Sast
+open Lexing
 open Printf
 
 type action = LLVM_IR | Compile | Ast | Sast
+
+let make_lexbuf file =
+  let lexbuf = Lexing.from_channel file in
+  let curr_p = {lexbuf.lex_curr_p with pos_lnum=1} in
+  {lexbuf with lex_curr_p = curr_p;}
+
+let parse_error lexbuf =
+  let s = lexeme_start_p lexbuf in
+  let f = lexeme_end_p lexbuf in
+    (fprintf stderr "Syntax error at line %d, char %d - %d: \"%s\" \n"
+    s.pos_lnum (s.pos_cnum - s.pos_bol)
+    (f.pos_cnum - f.pos_bol) (Lexing.lexeme lexbuf))
 
 let _ =
   let arg_len = Array.length Sys.argv in
@@ -22,9 +35,14 @@ let _ =
     else
       (Compile, open_in Sys.argv.(1))
     ) in
-  let lexbuf = Lexing.from_channel oscar
-  and stdlex = Lexing.from_channel (open_in "include/stdlib.oscar") in
-  let program = Parser.program Scanner.token lexbuf
+  let lexbuf = make_lexbuf oscar
+  and stdlex = make_lexbuf (open_in "include/stdlib.oscar") in
+  let program =
+    try
+      Parser.program Scanner.token lexbuf
+    with
+      Parsing.Parse_error ->
+        let () = ignore(parse_error lexbuf) in exit(1)
   and stdlib = Parser.program Scanner.token stdlex in
   let sprogram =
     try
