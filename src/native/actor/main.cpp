e034cc0022c26aa44301cf47c9e0b97455197964
Vlad Scherbich <vscherbich@squarespace.com>
2016-12-20 02:18:15 -0500

    started changing Actor pool impl [UNFINISHED]

--- src/native/actor/main.cpp
+++ src/native/actor/main.cpp
@@ -9,17 +9,7 @@ using namespace std;
 
 
 int main() {
-//    Ping ping;
-//    Pong pong;
-//
-//    ping.setPong(&pong);
-//
-//    StartMessage startMessage;
-//    ping.receive(&startMessage);
-//
-//    return 0;
-
-    Pool pool(1);
+    Pool<Ping> pool(1);
     pool.receive(new StartMessage());
 
     return 0;
--- src/native/actor/pool.hpp
+++ src/native/actor/pool.hpp
@@ -10,74 +10,82 @@
 
 #include "actor.hpp"
 #include "pingpong.hpp"
+using namespace std;
 
 
-//template <typename T>
+template <typename T>
 class Pool {
-    thread t;
+    vector<thread> workers;
+//    queue< function<void()> > tasks;
+    queue<Message*> tasks;
+
     condition_variable cv;
     mutex mx;
     bool tFinished;
 
-    vector<Ping> pings;
-    vector<Pong> pongs;
-
-    void consume() {
-        unique_lock<mutex> lck(mx);
-
-        while (true) {
-            while (q.empty())
-                cv.wait(lck);
-
-            int i = (count++) % pings.size();
-            Message* msg = q.front();
-            q.pop();
-
-            pings[i].receive(msg);
-        }
-    }
-
-public:
-    // circular buffer
-    vector<Actor*> cbuff;
-    int count;
-    int cap;
-
-    queue<Message*> q;
-
-    Pool(int cap=3) : count(0), cap(cap)
-    {
-        pings.resize(cap);
-        pongs.resize(cap);
-
-        // assign things
-        for (int i = 0; i < pings.size(); ++i) {
-            pings[i].setmaxTurns(1);
-            pings[i].setPong(&pongs[i]);
-        }
-
-        t = thread([=] { consume(); });
-    }
-
-    ~Pool() {
-        t.join();
-    }
-
-//    Pool(const vector<Actor*>& actors) : count(0) {
-//        cbuff = actors;
-//    }
-
-//    void receive(const vector<Message*>& msgs) {
-//        for (int i = 0; i < msgs.size(); ++i)
-//            int ndx = (count++) % cbuff.size();
-//            cbuff[ndx]->receive(msgs[i]);
+//    vector<Ping> pings;
+//    vector<Pong> pongs;
+//
+//    void consume() {
+//        unique_lock<mutex> lck(mx);
+//
+//        while (true) {
+//            while (q.empty())
+//                cv.wait(lck);
+//
+//            int i = (count++) % pings.size();
+//            Message* msg = q.front();
+//            q.pop();
+//
+//            pings[i].receive(msg);
 //        }
 //    }
 
+public:
+    Pool(int cap = 3) : tFinished(false) {
+        for (int i = 0; i < cap; ++i) {
+            workers.push_back(
+                    [this] {
+                        while (true) {
+                            std::function<void()> task;
+
+                            {
+                                std::unique_lock<std::mutex> lock(mx);
+                                cv.wait(lock,
+                                        [this] { return tFinished || !tasks.empty(); }
+                                );
+                                if (tFinished && tasks.empty())
+                                    return;
+
+                                // todo: compilation breaks here b/c Message is incompatible with function<void()>
+                                task = tasks.front();
+                                tasks.pop();
+                            }
+
+                            task();
+                        }
+                    }
+            );
+        }
+    }
+
+    ~Pool() {
+        {
+            unique_lock<mutex> lck(mx);
+            tFinished = true;
+        }
+
+        cv.notify_all();
+        for (thread& t : workers)
+            t.join();
+    }
+
+    // todo: this is equivalent of ThreadPool::enqueue
+    // todo: needs love
     void receive(Message* msg) {
         {
             unique_lock<mutex> lck(mx);
-            q.push(msg);
+            tasks.push(msg);
         }
 
         cv.notify_one();
